4fd2ad3aa63032ebb9489e337d7d8eabe251b443
RichardJavier1985 <richard.javier.1985@gmail.com>
2016-03-23 21:38:19 -0500

    Ejercicio de las frases locas

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var nombre = prompt ("Ingrese su nombre: ");
+var animal = prompt ("Ingrese su animal favorito: ");
+var verbo = prompt ("Ingrese un verbo: ");
+document.write("<h1>Los Amigos de Richard</h1>");
+document.write("<p>En la Upse "+nombre+" "+verbo+" como "+animal+" en celo.</p>");
+document.write("<p> Quien  "+verbo+" igual a "+animal+" ?...."+nombre+" .</p>");
+document.write("<p> Cuando "+verbo+" "+nombre+" y el(la) "+animal+" nadie ve la diferencia.</p>");
